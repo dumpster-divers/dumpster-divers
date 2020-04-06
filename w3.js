61eb7ed8029e67f32cf4ce20fb0405bc661a9107
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-04-06 22:19:44 +1000

    Remove tute exercises

--- w3.js
+++
@@ -1,6 +0,0 @@
-function getFormValue(){
-	var firstName = document.forms["formname"]["firstname"].value;
-	var lastName = document.forms["formname"]["lastname"].value;
-	alert("Hello "+firstName+" "+ lastName);
-}
-
