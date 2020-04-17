aa35a143460630a59f23e6db1cb5c595139c955d
Steven Nguyen <stevenhuyn@gmail.com>
2020-04-17 16:36:57 +1000

    changing index page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("<H1>Library System</H1>");
 });
-
 
 module.exports = router;
