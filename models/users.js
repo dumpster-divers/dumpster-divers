0f5b947b46e61ccdd0207c88cb121488e3446d56
winniwww <61858282+winniwww@users.noreply.github.com>
2020-04-21 19:56:26 +1000

    Create users.js

---
+++ models/users.js
@@ -0,0 +1,17 @@
+module.exports = [
+
+{
+  "id":"1",
+  "username":"winnie",
+  "dateJoined":"20/04",
+  "trashRecycled":"000",
+},
+
+{
+  "id":"2",
+  "username":"pooh",
+  "dateJoined":"20/04",
+  "trashRecycled":"020",
+},
+
+];
