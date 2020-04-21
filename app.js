5342bb9f674db5d5f4505bb9b2e2bb3cef9ac2d2
Steven N <stevenhuyn@gmail.com>
2020-04-21 23:07:49 +1000

    Setting up environment variables and launch script (#8) (#9)
    * Setting up environment variables and launch script (#8)
    
    * first commit
    
    * removed accidentally added whitespace
    
    * ci test fix
    
    * fixed "environment" typo
    
    * encoded # in connection string
    
    * updated password
    
    * added procfile
    
    * controller/route modification
    
    * changing index page
    
    * renamed stuffs
    
    * fixed structure of code
    
    * added a whitespace
    
    * updated readme
    
    * fixed readme
    
    * Update title
    
    * Install nodemon
    
    * Fix package merge conflict
    
    * Clean up app
    
    * fixed readme
    
    Co-authored-by: Matthew Lui <gamma032@gmail.com>

--- app.js
+++ app.js
@@ -1,56 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+require("./models");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var trashRouter = require('./routes/trash');
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// View engine
 app.set('view engine', 'jade');
 
-// Configure bodyparser to handle post requests
-app.use(bodyParser.urlencoded({extended: true}));
+// Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Connect to Mongoose and set connection variable
-mongoose.connect("mongodb://localhost:27017/restApiDB",
-    {useNewUrlParser: true, useUnifiedTopology: true});
+// Home page request
+const indexRouter = require("./routes/index")
+app.use("/", indexRouter)
 
-var db = mongoose.connection;
+// Trash count related requests
+const trashRouter = require("./routes/trashRouter");
+app.use("/api/trash", trashRouter);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/trash', trashRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// start app and listen for incoming requests on port
+app.listen(process.env.PORT || 3000, () => {
+  console.log("App is running on port " + (process.env.PORT || 3000));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
