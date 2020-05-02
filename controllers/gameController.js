1b1baee5a63b1882503a1e82fff5f2184813f6c4
Matthew Lui <gamma032@gmail.com>
2020-05-02 19:46:58 +1000

    Split functions

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -5,7 +5,7 @@ const Users = require('../models/Users');
 // game/add-session-stats: decrase the global count and increase user's total processed
 const addSessionStats = async (req, res) => {
   // Must pass in a count
-  if (req.body.count === undefined) res.status(400).send('No count provided');
+  if (req.body.count === undefined) return res.status(400).send('No count provided');
 
   // Get current count
   const trashCount = await Stats.findOne();
@@ -13,7 +13,7 @@ const addSessionStats = async (req, res) => {
 
   // Don't let count fall below 0
   remaining = Math.max(0, remaining - req.body.count);
-  console.log(remaining);
+
   // Decrease global count count
   Stats.updateOne({}, { globalRemaining: parseInt(remaining) }, {}, err => {
     if (err) console.log(err);
@@ -21,23 +21,7 @@ const addSessionStats = async (req, res) => {
  
   // Increment user count  (optional)
   if (req.body.username !== undefined) {
-    const user = await Users.find()
-
-    // Update procesesdTotal and get current record
-    const currentRecord = Users.findOneAndUpdate(
-      {username: req.body.username},
-      {$inc: {processedTotal: req.body.count}}, 
-      err => {if (err) console.log(err);}
-    ).processedRecord
-
-    // Update record if higher
-    if (req.body.count > currentRecord) {
-      Users.findOneAndUpdate(
-        {username: req.body.username},
-        {processedRecord: req.body.count},
-        err => {if (err) console.log(err);}
-      );
-     }
+    updateUserSessions(req.body.count, req.body.username);
   }
 
   return res.send();
@@ -57,6 +41,25 @@ const getRemaining = async (req, res) => {
     console.log(err)
     return res.send("Database query failed");
   }
+}
+
+
+const updateUserSessions = async (count, username) => {
+  // Update procesesdTotal and get current record
+  const user = await Users.findOneAndUpdate(
+    {username: username},
+    {$inc: {processedTotal: count}}, 
+    err => {if (err) console.log(err);}
+  ).select('processedRecord');
+
+  // Update record if higher
+  if (count > user.processedRecord) {
+    Users.findOneAndUpdate(
+      {username: username},
+      {$set: {processedRecord: count}},
+      err => {if (err) console.log(err);}
+    );
+   }
 }
 
 module.exports = {
