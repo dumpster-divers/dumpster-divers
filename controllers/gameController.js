161e4d6db1246fe2b2e7d4709d116580c9cba1eb
Matthew Lui <gamma032@gmail.com>
2020-05-02 18:42:48 +1000

    Update user counts with session

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -4,20 +4,42 @@ const Users = require('../models/Users');
 
 // game/add-session-stats: decrase the global count and increase user's total processed
 const addSessionStats = async (req, res) => {
+  // Must pass in a count
+  if (req.body.count === undefined) res.status(400).send('No count provided');
+
   // Get current count
-  const trashCount = await Stats.find();
-  let remaining = trashCount[0].globalRemaining;
+  const trashCount = await Stats.findOne();
+  let remaining = trashCount.globalRemaining;
 
   // Don't let count fall below 0
   remaining = Math.max(0, remaining - req.body.count);
   console.log(remaining);
   // Decrease global count count
   Stats.updateOne({}, { globalRemaining: parseInt(remaining) }, {}, err => {
-    if (err) {
-      console.log(err);
-    }
+    if (err) console.log(err);
   });
-  
+ 
+  // Increment user count  (optional)
+  if (req.body.username !== undefined) {
+    const user = await Users.find()
+
+    // Update procesesdTotal and get current record
+    const currentRecord = Users.findOneAndUpdate(
+      {username: req.body.username},
+      {$inc: {processedTotal: req.body.count}}, 
+      err => {if (err) console.log(err);}
+    ).processedRecord
+
+    // Update record if higher
+    if (req.body.count > currentRecord) {
+      Users.findOneAndUpdate(
+        {username: req.body.username},
+        {processedRecord: req.body.count},
+        err => {if (err) console.log(err);}
+      );
+     }
+  }
+
   return res.send();
 };
 
