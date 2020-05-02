729dacd342d753e09a3bdc38d07376e07f4db478
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-02 21:25:24 +1000

    added logo and title to front page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/dumpster_diver.png';
 import './App.css';
 import TallyCounter from './TallyCounter'
 
 function App() {
   return (
     <div className="App">
+      <h1>Dumpster Divers</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
