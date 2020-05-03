ecb3e0d2810db14037faae7d59decbfcff4eec60
Matthew Lui <gamma032@gmail.com>
2020-05-03 13:48:29 +1000

    Bugfix: Duplicate processedTotal increment

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -16,9 +16,10 @@ const addSessionStats = async (req, res) => {
   remaining = Math.max(0, remaining - req.body.count);
 
   // Decrease global count count
-  Stats.updateOne({}, { globalRemaining: parseInt(remaining) }, {}, err => {
-    if (err) console.log(err);
-  });
+  Stats.updateOne({}, 
+    {globalRemaining: parseInt(remaining) }, 
+    {}, err => { if (err) console.log(err);}
+  );
  
   // Increment user count  (optional)
   if (req.body.username !== undefined) {
@@ -50,11 +51,11 @@ const getRemaining = async (req, res) => {
 
 const updateUserSessions = async (count, username) => {
   // Update procesesdTotal and get current record
+  console.log(count);
   const user = await Users.findOneAndUpdate(
     {username: username},
-    {$inc: {processedTotal: count}}, 
-    err => {if (err) console.log(err);}
-  ).select('processedRecord');
+    {$inc: {processedTotal: count}}
+  );
 
   // Update record if higher
   if (count > user.processedRecord) {
