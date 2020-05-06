3a436204ff67be37e70017b3d86e9638a4b01bee
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-05-06 14:33:47 +1000

    Add containers to all the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,28 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import FrontPage from "./frontpage/FrontPage";
-import Game from "./Game";
-import Signup from "./Signup";
+import Game from "./game/Game";
+import Signup from "./signup/Signup";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/game">
-            <Game />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/">
-            <FrontPage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>Dumpster Divers</h1>
+      <header className="App-header">
+        <Router>
+          <Switch>
+            <Route path="/game">
+              <Game />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/">
+              <FrontPage />
+            </Route>
+          </Switch>
+        </Router>
+      </header>
+    </div>
   );
 }
