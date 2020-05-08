d6bb43fa2bebc7531d87b65e9505f3fb5a6b2388
Matthew Lui <gamma032@gmail.com>
2020-05-08 15:17:22 +1000

    Add HeaderText component

--- frontend/src/frontpage/FrontPage.js
+++ frontend/src/frontpage/FrontPage.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "../assets/dumpster_diver.png";
 import "../App.css";
 import ActionButton from "../shared/ActionButton";
+import HeaderText from "../shared/HeaderText";
+
 
 import { Link } from "react-router-dom";
 import GameContainer from "../shared/GameContainer";
@@ -9,6 +11,7 @@ import GameContainer from "../shared/GameContainer";
 const FrontPage = () => {
   return (
     <GameContainer>
+      <HeaderText>Dumpster Divers</HeaderText>
       <img src={logo} className="App-logo" alt="logo" />
       <Link to="/signup">Sign Up</Link>
       <ActionButton to="/game" />
