68facbb7c31925dc5002e1ca0eb8f52b8c157888
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-08 17:03:12 +1000

    checking out PopOver

--- frontend/src/frontpage/FrontPage.js
+++ frontend/src/frontpage/FrontPage.js
@@ -6,47 +6,16 @@ import ActionButton from "../shared/ActionButton";
 import { Link } from "react-router-dom";
 import GameContainer from "../shared/GameContainer";
 
-import ReactHover from 'react-hover';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import ReactHover       from 'react-hover';
+import { makeStyles }   from '@material-ui/core/styles';
+import Card             from '@material-ui/core/Card';
+import CardActions      from '@material-ui/core/CardActions';
+import CardContent      from '@material-ui/core/CardContent';
+import Button           from '@material-ui/core/Button';
+import Typography       from '@material-ui/core/Typography';
+import MouseOverPopover from "../shared/ModalContainer";
 
 const FrontPage = () => {
-  const options = {
-    followCursor: true,
-    shiftX: 10,
-    shiftY: -10
-  }
-
-  const classes = makeStyles();
-  const bull = <span className={classes.bullet}>•</span>;
-
-  const card = (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
 
   return (
     <div className="App">
---
+++ frontend/src/shared/ModalContainer.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function MouseOverPopover(props) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  return (
+    <div>
+      <div
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+        >
+        {props.children}
+      </div>
+      <Popover
+        id="mouse-over-popover"
+        className={classes.popover}
+        classes={{
+          paper: classes.paper,
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <Typography>I use Popover.</Typography>
+      </Popover>
+    </div>
+  );
+}
