b3f7afc508b4e254d003eebca04b3de8e167c1a7
winniwww <61858282+winniwww@users.noreply.github.com>
2020-05-10 02:27:11 +1000

    save progress

--- frontend/src/frontpage/FrontPage.js
+++ frontend/src/frontpage/FrontPage.js
@@ -12,16 +12,34 @@ const FrontPage = () => {
       <h1>Dumpster Divers</h1>
       <header className="App-header">
         <GameContainer>
-          <img src={logo} className="App-logo" alt="logo" />
           <div class="Small-link-div">
             <Link to="/signup" class="Small-link">Sign Up</Link>
             <Link to="/signin" class="Small-link">Sign In</Link>
           </div>
-          <ActionButton to="/game" buttonText={"Dive In!"}/>
+          <div id="dumpster-diver">
+            <img src="dumpster_diver.png" alt="dumpster diver mascot"/>
+          </div>
+          <div id="frontpage-button">
+            <ActionButton to="/game" buttonText={"Dive In!"} />
+          </div>
         </GameContainer>
       </header>
     </div>
   );
 };
+
+function moveDiver() {
+  var elem = document.getElementById("dumpster-diver");
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + "px";
+    }
+  }
+}
 
 export default FrontPage;
