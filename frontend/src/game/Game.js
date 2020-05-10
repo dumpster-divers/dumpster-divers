cc6525d434ff3e295cd08f538ddda409f07315c4
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-10 11:31:59 +1000

    added a popover example of game

--- frontend/src/game/Game.js
+++ frontend/src/game/Game.js
@@ -1,11 +1,32 @@
-import React from "react";
-import GameContainer from "../shared/GameContainer";
+import React          from "react";
+import GameContainer  from "../shared/GameContainer";
+import GenericPopover from "../shared/GenericPopover";
+import {makeStyles}   from "@material-ui/core/styles";
 
 const Game = () => {
 	return (
 	  <GameContainer>
-			<h1>This will be the <em>GAME</em> page eventually</h1>
+			<GenericPopover popover={HeaderPopover()}>
+				<h1>This will be the <em>GAME</em> page eventually</h1>
+			</GenericPopover>
 		</GameContainer>
+	);
+}
+
+const HeaderPopover = () => {
+	const useStyles = makeStyles((theme) => ({
+		popover: {
+			margin: "20px"
+		}
+	}));
+
+	const classes = useStyles();
+
+	 return (
+		<div className={classes.popover}>
+			<h1>Hey this is the popover</h1>
+			<p>And this is the paragraph below!</p>
+		</div>
 	);
 }
 
