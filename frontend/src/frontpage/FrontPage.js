9a0a9b3391959c865265bc3a2514d34888069715
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-10 11:33:43 +1000

    removed unecessary imports from front page

--- frontend/src/frontpage/FrontPage.js
+++ frontend/src/frontpage/FrontPage.js
@@ -3,12 +3,6 @@ import logo from "../assets/dumpster_diver.png";
 import "../App.css";
 import ActionButton from "../shared/ActionButton";
 
-import { Link } from "react-router-dom";
-import GameContainer from "../shared/GameContainer";
-
-import MouseOverPopover from "../shared/GenericPopover";
-import {makeStyles}     from "@material-ui/core/styles";
-
 const FrontPage = () => {
   return (
     <div className="App">
