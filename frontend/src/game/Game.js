703c9a2a7556e6ba33b43471c63ec19452a46a1e
Matthew Lui <gamma032@gmail.com>
2020-05-11 23:46:58 +1000

    Move interval into component

--- frontend/src/game/Game.js
+++ frontend/src/game/Game.js
@@ -3,21 +3,20 @@ import GameContainer from "../shared/GameContainer";
 import Timer from "./Timer"
 
 const Game = () => {
-	// Dummy counter logic
+/* 	// Dummy counter logic
 	const counterDuration = 30;
 	// Use datetime over subtracting one to continue the timer when tabbed out
 	const [initTime] = useState(Date.now() / 1000);
 	const [counter, setCounter] = useState(counterDuration);
 
 	setInterval(() => {
-		setCounter(Math.max(Math.round(counterDuration - (Date.now() / 1000 - initTime)), 0));
+		setCounter(Math.max(counterDuration - (Date.now() / 1000 - initTime), 0));
 	}, 1000);
-
+ */
 	return (
 	  <GameContainer>
 		<h1>This will be the <em>GAME</em> page eventually</h1>
-			<p>{counter}</p>
-		<Timer count={counter}/>
+		<Timer maxCount={30}/>
 	</GameContainer>
 	);
 }
--- frontend/src/game/Timer.js
+++ frontend/src/game/Timer.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-const Timer = ({count}) => {
+import { lighten, createStyles, makeStyles} from '@material-ui/core/styles';
+import LinearProgress from "@material-ui/core/LinearProgress";
+
+const Timer = ({maxCount}) => {
+    //const proportion = ((currentCount/maxCount)*100).toFixed(2);
+    //console.log(proportion)
+    const [proportion, setProportion] = useState(100);
+    const [initTime] = useState(Date.now() / 1000);
+
+    useEffect(() => {
+    const interval = setInterval(() => {
+        const count = Math.max(maxCount - (Date.now() / 1000 - initTime), 0);
+        setProportion(((count/maxCount)*100).toFixed(2));
+    }, 100);
+    
+    // Cleanup by stopping interval timer
+    return () => clearInterval(interval);
+    }, []); 
+    //const useStyles = 
+ 
+    //console.log(;
     return (
-    <p>{count}</p>
+        <div id = "timer">
+            <LinearProgress variant="determinate" value={proportion}/>
+            <p></p>
+        </div>
     )
 }
 
