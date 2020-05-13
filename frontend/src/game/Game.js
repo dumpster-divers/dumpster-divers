62535b1be0b203a9385b596b416995ba2c6a3454
winniwww <61858282+winniwww@users.noreply.github.com>
2020-05-13 18:18:45 +1000

    modal finished

--- frontend/src/game/Game.js
+++ frontend/src/game/Game.js
@@ -2,23 +2,25 @@ import React from "react";
 import GameContainer  from "../shared/GameContainer";
 import GenericPopover from "../shared/GenericPopover";
 import {makeStyles}   from "@material-ui/core/styles";
-import IncorrectBin from "./IncorrectBin";
+import IncorrectBinModal from "./IncorrectBinModal";
 
+//dummy trash object for now
+const trashApple = {
+	id: 1,
+	info: "this is why you're wrong blah blah blah blah",
+}
 
 const Game = () => {
 	return (
 	  <GameContainer>
-		<div className="yellow-part" id="yellow-part">
 			<GenericPopover popover={HeaderPopover()}>
 				<h1>This will be the <em>GAME</em> page eventually</h1>
 			</GenericPopover>
-			<div>
-			  <IncorrectBin/>
-			</div>
-		</div>
+			<IncorrectBinModal trashInfo={trashApple.info}/>
 		</GameContainer>
 	);
 }
+
 
 const HeaderPopover = () => {
 	const useStyles = makeStyles((theme) => ({
