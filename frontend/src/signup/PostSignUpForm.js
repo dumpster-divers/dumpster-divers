33286ec6f5d3f20b116d8bf8f2e42c23f751f68e
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-05-14 20:09:01 +1000

    Add copy username button

--- frontend/src/signup/PostSignUpForm.js
+++ frontend/src/signup/PostSignUpForm.js
@@ -1,17 +1,25 @@
 import TextEntry from "../shared/TextEntry";
 import React from "react";
 import Cookies from "js-cookie";
+import ActionButton from "../shared/ActionButton";
 
 const PostSignUpForm = () => {
+  const copyUsername = () => {
+    navigator.clipboard.writeText(Cookies.get("username"))
+      .then(null, null)
+
+  };
   return (
     <>
       <TextEntry
-        value={Cookies.get('username')}
+        value={Cookies.get("username")}
         onChange={null}
         isDisabled={true}
       />
+      <div style={{marginTop: "15px"}}>
+        <ActionButton buttonText={"Copy Username"} onClick={copyUsername} />
+      </div>
     </>
-);
-
-}
+  );
+};
 export default PostSignUpForm;
