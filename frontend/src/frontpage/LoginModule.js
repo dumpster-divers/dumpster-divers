ad524030f91e4b2c39e7df06858d134443e3774e
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-05-15 20:19:15 +1000

    Fix warnings

--- frontend/src/frontpage/LoginModule.js
+++ frontend/src/frontpage/LoginModule.js
@@ -2,13 +2,13 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {getName, isLoggedIn, logoutUser} from "../utilities/userManager";
 
-const LoginModule = ({}) => {
+const LoginModule = () => {
 
   const loggedInMessage = (
     <div className="Small-link-div">
       <div className="Small-link">
         Welcome back, {getName()}!
-        <a style={{marginLeft: "10px"}} href="" onClick={logoutUser}>
+        <a style={{marginLeft: "10px"}} href="/" onClick={logoutUser}>
           Logout
         </a>
       </div>
