2789eb1ef0303fafe6a073e4e792583ba8780e56
Matthew Lui <gamma032@gmail.com>
2020-05-16 23:11:15 +1000

    Ipad fixes

--- frontend/src/shared/DiverCard.js
+++ frontend/src/shared/DiverCard.js
@@ -1,11 +1,23 @@
 import React from "react";
 import diver_cert_card from "../assets/diver_cert_card.png";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles, createMuiTheme } from "@material-ui/core/styles";
 import { getName } from "../utilities/userManager";
 
 const DiverCard = ({ points = 0 }) => {
   const nameText = getName() ? "Name: \n" + getName() : "";
   const pointsText = "Points: " + points;
+
+  const theme = createMuiTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 960,
+        lg: 1280,
+        xl: 1920,
+      },
+    },
+  })
 
   const useStyles = makeStyles((theme) =>
     createStyles({
@@ -22,16 +34,14 @@ const DiverCard = ({ points = 0 }) => {
         fontFamily: "Roboto",
         zIndex: "9",
         color: "#333436",
-        [theme.breakpoints.up('md')]: {
+        fontSize: "15px",
+        marginTop: "50px",
+        marginLeft: "45px",
+        [theme.breakpoints.up('sm')]: {
           fontSize: "25px",
           marginTop: "75px",
           marginLeft: "70px"
 
-        },
-        [theme.breakpoints.down('md')]: {
-          fontSize: "15px",
-          marginTop: "50px",
-          marginLeft: "45px"
         },
         textAlign: "left",
         fontWeight: "bold",
