6811092df24fb9c9f23dc95e4b99ae8f486fad5e
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-16 16:06:53 +1000

    fix dive in button to be able to reload GamePage

--- frontend/src/game/PostGame.js
+++ frontend/src/game/PostGame.js
@@ -3,11 +3,13 @@ import GameContainer  from "../shared/GameContainer";
 import diver_cert_card from "../assets/diver_cert_card.png";
 import ActionButton from "../shared/ActionButton";
 import ExitGameButton from "./ExitGameButton";
-import { Link } from "react-router-dom";
-
 
 
 const PostGame = () => {
+  const handleClick = () => {
+    window.location.reload();
+  }
+
 	return (
 		<GameContainer>
       <div className='yellow-part'>
@@ -15,9 +17,7 @@ const PostGame = () => {
         <p className='postgame-content'>we've just updated your new total on your diver certification card</p>
         <img className='diver-cert-card' src={diver_cert_card} alt="diver certification card" />
         <div className='play-again-button'>
-          <Link to="/game">
-            <ActionButton to="/game" buttonText={"Dive Again"} />
-          </Link>
+          <ActionButton onClick={handleClick} to="/game" buttonText={"Dive Again"} />
         </div>
         <ExitGameButton/>
       </div>
