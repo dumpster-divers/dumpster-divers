9e48b1ea4b1822656ce5d78784ba29891074e321
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-05-16 17:55:35 +1000

    Clean up layout

--- frontend/src/game/PostGame.js
+++ frontend/src/game/PostGame.js
@@ -1,28 +1,35 @@
 import React from "react";
-import GameContainer  from "../shared/GameContainer";
+import GameContainer from "../shared/GameContainer";
 import ActionButton from "../shared/ActionButton";
 import ExitGameButton from "./ExitGameButton";
 import DiverCard from "../shared/DiverCard";
 
-
-const PostGame = ({points}) => {
+const PostGame = ({ points }) => {
   const handleClick = () => {
     window.location.reload();
-  }
+  };
 
-	return (
-		<GameContainer>
-      <div className='yellow-part'>
-			  <h1 className='heading-postgame'>Woah! What a dive!</h1>
-        <p className='postgame-content'>We've just updated your new total on your diver certification card</p>
-        <DiverCard points = {points}/>
-        <div className='play-again-button'>
-          <ActionButton onClick={handleClick} to="/game" buttonText={"Dive Again"} />
+  return (
+    <GameContainer>
+      <div className="yellow-part">
+        <h1 className="heading-postgame">Woah! What a dive!</h1>
+        <p className="postgame-content">
+          We've just updated your new total on your diver certification card
+        </p>
+        <div style={{ marginTop: "8%" }}>
+          <DiverCard points={points} />
         </div>
-        <ExitGameButton/>
+        <div className="frontpage-button">
+          <ActionButton
+            onClick={handleClick}
+            to="/game"
+            buttonText={"Dive Again"}
+          />
+        </div>
+        <ExitGameButton />
       </div>
-		</GameContainer>
-	);
-}
+    </GameContainer>
+  );
+};
 
 export default PostGame;
