1ef6d2ed6580ff60ad6c51dec028cf8eb7e7fe32
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-16 15:39:33 +1000

    removed unecessary imports in util

--- frontend/src/game/GameUtil.js
+++ frontend/src/game/GameUtil.js
@@ -1,10 +1,3 @@
-import canSource        from "../assets/can.png";
-import strawSource      from "../assets/straw.png";
-import appleSource      from "../assets/apple.png";
-import milkBottleSource from "../assets/milk_bottle.png";
-import newspaperSource  from "../assets/newspaper.png";
-import waterBottle      from "../assets/water_bottle.png";
-
 const getTrash = () => {
   return TRASH[Math.floor(Math.random() * TRASH.length)]
 }
