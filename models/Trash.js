a94aeab504780568c721f3e855e41bea01589507
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-16 17:47:11 +1000

    hooking up backend trash to database

--- models/Trash.js
+++ models/Trash.js
@@ -1,44 +1,12 @@
-const trash = [
-  {
-    id: 1,
-    name: "plastic bottle",
-  },
-  {
-    id: 2,
-    name: "glass bottle",
-  },
-  {
-    id: 3,
-    name: "plastic wrapping",
-  },
-  {
-    id: 4,
-    name: "coffee cup",
-  },
-  {
-    id: 5,
-    name: "newspaper",
-  },
-  {
-    id: 6,
-    name: "chips packaging",
-  },
-  {
-    id: 7,
-    name: "plastic straw",
-  },
-  {
-    id: 8,
-    name: "aluminium foil",
-  },
-  {
-    id: 9,
-    name: "cling wrap",
-  },
-  {
-    id: 10,
-    name: "old clothes",
-  },
-];
+const mongoose = require("mongoose");
 
-module.exports = trash;
+// Schema for api/users
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    index: true
+  },
+  recyclable: Boolean
+});
+
+module.exports = mongoose.model("Trash", userSchema, "Trash");
