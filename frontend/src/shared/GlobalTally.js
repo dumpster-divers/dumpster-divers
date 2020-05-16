ecd0a45ad6da254896b86428fdd2336106068fb6
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-05-16 23:10:22 +1000

    Theme global tally component

--- frontend/src/shared/GlobalTally.js
+++ frontend/src/shared/GlobalTally.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ScoreCounter from "../game/ScoreCounter";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 
 const GlobalTally = () => {
   const [tally, setTally] = useState(0);
@@ -13,6 +14,35 @@ const GlobalTally = () => {
     fetchData().then(null);
   }, []);
 
-  return <ScoreCounter score={tally}/>
+  const useStyle = makeStyles(() =>
+    createStyles({
+      wrapper: {
+        display: "flex",
+        flexDirection: "row",
+        width: "auto",
+        maxWidth: "500px",
+        background: "#ffebb0",
+        padding: "10px 0",
+        borderRadius: "20px",
+        marginLeft: "auto",
+        marginRight: "auto"
+      },
+      text: {
+        color: "#4d4125",
+        fontFamily: "Roboto, sans-serif",
+        fontSize: "30px",
+        marginLeft: "30px"
+      }
+    })
+  );
+
+  const classes = useStyle();
+
+  return (
+    <div className={classes.wrapper}>
+      <span className={classes.text}>Global Rubbish Remaining</span>
+      <ScoreCounter score={tally} />
+    </div>
+  );
 };
 export default GlobalTally;
