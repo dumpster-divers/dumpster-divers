cc882b01e69183cf89fbb63bd7da67255d96c40e
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-16 15:37:20 +1000

    added new trash items

--- frontend/src/game/GameUtil.js
+++ frontend/src/game/GameUtil.js
@@ -1,3 +1,10 @@
+import canSource        from "../assets/can.png";
+import strawSource      from "../assets/straw.png";
+import appleSource      from "../assets/apple.png";
+import milkBottleSource from "../assets/milk_bottle.png";
+import newspaperSource  from "../assets/newspaper.png";
+import waterBottle      from "../assets/water_bottle.png";
+
 const getTrash = () => {
   return TRASH[Math.floor(Math.random() * TRASH.length)]
 }
@@ -10,6 +17,22 @@ const TRASH = [
   {
     name: "straw",
     recyclable: false
+  },
+  {
+    name: "apple",
+    recyclable: false
+  },
+  {
+    name: "milkBottle",
+    recyclable: true
+  },
+  {
+    name: "newspaper",
+    recyclable: true
+  },
+  {
+    name: "waterBottle",
+    recyclable: true
   }
 ]
 
