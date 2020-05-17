e950b2755692eea3398a1e147dc09aa82d57203b
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-17 15:21:57 +1000

    fixed import

--- frontend/src/user/UpdateUserForm.js
+++ frontend/src/user/UpdateUserForm.js
@@ -1,47 +1,45 @@
-import React, { useState }    from "react";
-import {getName, getUsername, isLoggedIn} from "../utilities/userManager";
-import updateUser             from "./UpdateUserApi";
-import Button                 from "@material-ui/core/Button";
-import Cookies                from "js-cookie";
-import TextEntry              from "../shared/TextEntry";
-import { Redirect }           from "react-router-dom";
-
+import React, { useState } from "react";
+import { getName, getUsername, isLoggedIn } from "../utilities/userManager";
+import updateUser from "./UpdateUserApi";
+import Button from "@material-ui/core/Button";
+import Cookies from "js-cookie";
+import TextEntry from "../shared/TextEntry";
+import { Redirect } from "react-router";
 
 export default function UpdateUserForm() {
-
-  if (!isLoggedIn()) {
-    <Redirect to="/" />
-  }
-
   const [inputName, setInputName] = useState(getName());
   const [username, setUsername] = useState(getUsername());
+
+  if (!isLoggedIn()) {
+    return <Redirect to="/" />;
+  }
 
   function onSubmit() {
     // call update user function
     updateUser({
       username: username,
-      name: inputName
-    }).then(res => res.json())
+      name: inputName,
+    })
+      .then((res) => res.json())
       .then((res) => {
-      Cookies.set("name", res.name);
-      setUsername(res.name);
-    }).then(res => window.location.reload());
+        Cookies.set("name", res.name);
+        setUsername(res.name);
+      })
+      .then((res) => window.location.reload());
   }
 
   return (
     <div>
       <form>
-        <p className='update-name'>Enter your new nickname</p>
+        <p className="update-name">Enter your new nickname</p>
         <TextEntry
           value={inputName}
-          onChange={event => {
+          onChange={(event) => {
             setInputName(event.target.value);
           }}
           placeholderText={username}
         />
-        <Button onClick={onSubmit}>
-          Update
-        </Button>
+        <Button onClick={onSubmit}>Update</Button>
       </form>
     </div>
   );
