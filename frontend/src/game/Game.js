a5a82047271746da26602fea0b138a1c5ab5e499
Matthew Lui <gamma032@gmail.com>
2020-05-17 14:09:34 +1000

    Update GIF

--- frontend/src/game/Game.js
+++ frontend/src/game/Game.js
@@ -30,7 +30,7 @@ const Game = ({ points, setPoints, setShowGame }) => {
   const isIncorrectModalOpenRef = useRef(isIncorrectModalOpen);
   isIncorrectModalOpenRef.current = isIncorrectModalOpen;
 
-  const GAME_DURATION = 10;
+  const GAME_DURATION = 1000;
 
   // Handling touch vs mouse dragging
   const backend = isMobile() ? TouchBackend : Backend;
