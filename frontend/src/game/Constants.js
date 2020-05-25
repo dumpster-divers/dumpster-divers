94ee5445d6bc61bc36321dbff94002ddbc37ded7
Steven Nguyen <stevenhuyn@gmail.com>
2020-05-25 18:26:18 +1000

    added trash to constants file

--- frontend/src/game/Constants.js
+++ frontend/src/game/Constants.js
@@ -4,6 +4,17 @@ import appleSource from "../assets/apple.png";
 import milkBottleSource from "../assets/milk_bottle.png";
 import newspaperSource from "../assets/newspaper.png";
 import waterBottleSource from "../assets/water_bottle.png";
+import chipsSource from "../assets/chips.png";
+import coffeeCupSource from "../assets/coffee_cup.png";
+import glassBottleSource from "../assets/glass_bottle.png";
+import greasyPizzaBoxSource from "../assets/greasy_pizza_box.png";
+import iceCreamTubSource from "../assets/icecream_tub.png";
+import ketchupSource from "../assets/ketchup.png";
+import letterSource from "../assets/letter.png";
+import pizzaBoxSource from "../assets/pizza_box.png";
+import plasticBagSource from "../assets/plastic_bag.png";
+import popsicleStickSource from "../assets/popsicle_stick.png";
+import toiletRollSource from "../assets/toilet_roll.png";
 
 export const ItemTypes = {
   can: canSource,
@@ -12,4 +23,15 @@ export const ItemTypes = {
   milkBottle: milkBottleSource,
   newspaper: newspaperSource,
   waterBottle: waterBottleSource,
+  glassBottle: glassBottleSource,
+  chips: chipsSource,
+  coffeeCup: coffeeCupSource,
+  greasyPizzaBox: greasyPizzaBoxSource,
+  iceCreamTub: iceCreamTubSource,
+  ketchup: ketchupSource,
+  letter: letterSource,
+  pizzaBox: pizzaBoxSource,
+  plasticBag: plasticBagSource,
+  popsicleStick: popsicleStickSource,
+  toiletRoll: toiletRollSource,
 };
