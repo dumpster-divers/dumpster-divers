9560b1d6864b808ada03a8a4ddc287f5d01f6be5
winniwww <61858282+winniwww@users.noreply.github.com>
2020-05-27 03:14:18 +1000

    made a portal to user homepage

--- frontend/src/frontpage/LoginModule.js
+++ frontend/src/frontpage/LoginModule.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { getName, isLoggedIn, logoutUser } from "../utilities/userManager";
+import UserButton from "./UserButton";
+import "./frontPageStyles.css";
 
 const LoginModule = () => {
   const loggedInMessage = (
-    <div className="Small-link-div">
-      <div className="Small-link">
-        Welcome back, {getName()}!
-        <a style={{ marginLeft: "10px" }} href="/" onClick={logoutUser}>
-          Logout
-        </a>
+    <div>
+      <div className="Small-link-div">
+        <div className="Small-link">
+          Welcome back, {getName()}!
+          <a style={{ marginLeft: "10px" }} href="/" onClick={logoutUser}>
+            Logout
+          </a>
+        </div>
+      </div>
+      <div className="user-button">
+        <UserButton />
       </div>
     </div>
   );
---
+++ frontend/src/frontpage/UserButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserButton = function () {
+  return (
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <a href="/UserHomepage">
+        <circle cx="24" cy="24" r="24" fill="#B8D1D7"/>
+        <path d="M24 27.5C16.5 27.5 9.16667 34.3333 5.5 39C8.5 43.5 15.5 48 24 48C32.5 48 40 43.5 43.5 38C39.5 33.6667 31.5 27.5 24 27.5Z" fill="#EBEBEB"/>
+        <circle cx="24" cy="15" r="10" fill="#EBEBEB"/>
+      </a>
+    </svg>
+  );
+};
+
+export default UserButton;
