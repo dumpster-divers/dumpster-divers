1a191dc1075de477d59f11b510acf99144b0d007
winniwww <61858282+winniwww@users.noreply.github.com>
2020-05-27 03:27:22 +1000

    updated diver card on postgame screen

--- frontend/src/shared/DiverCard.js
+++ frontend/src/shared/DiverCard.js
@@ -1,11 +1,36 @@
 import React from "react";
 import diver_cert_card from "../assets/diver_cert_card.png";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
-import { getName } from "../utilities/userManager";
+import { getName, getUsername } from "../utilities/userManager";
+import { useState, useEffect } from "react";
 
 const DiverCard = ({ points = 0 }) => {
+  //display this while waiting for fetch call
+  const loadingUserStats = {"processedTotal":"loading...", "processedRecord":"loading..."}
+
+  const [userStats, setUserStats] = useState(loadingUserStats);
+  const [userRank, setUserRank] = useState("loading...");
+
+  //fetch data from api and update userRank and userStats state
+  useEffect(() => {
+    async function fetchUser() {
+      const username = getUsername();
+      const response = await fetch(`/api/stats/user-highscore?username=${username}`);
+      const data = await response.json();
+      const userStats = data.user;
+      setUserStats(userStats);
+      const userRank = data.userRank;
+      setUserRank(userRank);
+    }
+    fetchUser();
+  }, []);
+
+  //construct strings that will be displayed on diver card
   const nameText = getName() ? "Name: \n" + getName() : "";
   const pointsText = "Points: " + points;
+  const currentTotal = "Total: " + userStats.processedTotal;
+  const currentRecord = "Record: " + userStats.processedRecord;
+  const currentRank = "Global Rank: " + userRank;
 
   const useStyles = makeStyles((theme) =>
     createStyles({
@@ -22,11 +47,11 @@ const DiverCard = ({ points = 0 }) => {
         fontFamily: "Roboto",
         zIndex: "9",
         color: "#333436",
-        fontSize: "15px",
+        fontSize: "20px",
         marginTop: "40px",
         [theme.breakpoints.up("sm")]: {
-          fontSize: "25px",
-          marginTop: "60px",
+          fontSize: "20px",
+          marginTop: "65px",
         },
         textAlign: "left",
         fontWeight: "bold",
@@ -34,6 +59,7 @@ const DiverCard = ({ points = 0 }) => {
     })
   );
 
+  //render the diver card
   const classes = useStyles();
   return (
     <div className={classes.wrapper}>
@@ -41,6 +67,12 @@ const DiverCard = ({ points = 0 }) => {
         {nameText}
         <br />
         {pointsText}
+        <br />
+        {currentTotal}
+        <br />
+        {currentRecord}
+        <br />
+        {currentRank}
       </div>
       <img
         className={classes.card}
