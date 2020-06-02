d3e1a131e60c999edd2e0ace8bdffe204387eb45
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-06-02 15:24:10 +1000

    Change "Welcome to Dumpster Divers" for users

--- frontend/src/frontpage/FrontPage.js
+++ frontend/src/frontpage/FrontPage.js
@@ -11,8 +11,13 @@ import "./frontPageStyles.css";
 import GlobalTally from "../shared/GlobalTally";
 import { isMobile } from "../utilities/display";
 import UserButton from "./UserButton";
+import { getName, isLoggedIn } from "../utilities/userManager";
 
 const FrontPage = () => {
+  let welcomeMessage = isLoggedIn()
+    ? "Welcome back, " + getName() + "!"
+    : "Welcome to Dumpster Divers!";
+
   return (
     <GameContainer>
       <div id="user-button">
@@ -20,16 +25,17 @@ const FrontPage = () => {
       </div>
       <div className="dive-in">
         <GlobalTally />
-        <h1 className="heading-homepage">Welcome to Dumpster Divers!</h1>
-        <p className="about-us">
-          {!isMobile() &&
-            "Did you know that 61% of Australians are unclear on what could be " +
-              "recycled? It may not be as easy as you think! Putting a recyclable item " +
-              "in the incorrect bin or not treating a contaminated item correctly means " +
-              "that recyclable items end up in the landfill. Dumpster Divers is here to " +
-              "teach you all about household rubbish sorting in a fun way. Dive in with " +
-              "us, you might be surprised!"}
-        </p>
+        <h1 className="heading-homepage">{welcomeMessage}</h1>
+        {!isMobile() && (
+          <p className="about-us">
+            Did you know that 61% of Australians are unclear on what could be
+            recycled? It may not be as easy as you think! Putting a recyclable
+            item in the incorrect bin or not treating a contaminated item
+            correctly means that recyclable items end up in the landfill.
+            Dumpster Divers is here to teach you all about household rubbish
+            sorting in a fun way. Dive in with us, you might be surprised!
+          </p>
+        )}
         <div id="mascot">
           <img src={logo} alt="logo" />
         </div>
@@ -39,7 +45,9 @@ const FrontPage = () => {
           </Link>
         </div>
       </div>
-      <LoginModule />
+      <div className="loginButton">
+        <LoginModule />
+      </div>
     </GameContainer>
   );
 };
--- frontend/src/frontpage/LoginModule.js
+++ frontend/src/frontpage/LoginModule.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { getName, isLoggedIn, logoutUser } from "../utilities/userManager";
 import "./frontPageStyles.css";
+import { isMobile } from "../utilities/display";
 
 const LoginModule = () => {
   const loggedInMessage = (
     <div>
       <div className="Small-link-div">
         <div className="Small-link">
-          Welcome back, {getName()}!
+          {!isMobile() && "Welcome back, " + getName() + "!"}
           <a style={{ marginLeft: "10px" }} href="/" onClick={logoutUser}>
             Logout
           </a>
