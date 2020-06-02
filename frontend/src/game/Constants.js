b85ec6a7357c297652189e6ce54030912a3707cb
Steven Nguyen <stevenhuyn@gmail.com>
2020-06-03 02:51:33 +1000

    updated graphics

--- frontend/src/game/Constants.js
+++ frontend/src/game/Constants.js
@@ -5,16 +5,19 @@ import milkBottleSource from "../assets/milk_bottle.png";
 import newspaperSource from "../assets/newspaper.png";
 import waterBottleSource from "../assets/water_bottle.png";
 import chipsSource from "../assets/chips.png";
-import coffeeCupSource from "../assets/coffee_cup.png";
+import coffeeCupSource from "../assets/coffeeCup.png";
 import glassBottleSource from "../assets/glass_bottle.png";
 import greasyPizzaBoxSource from "../assets/greasy_pizza_box.png";
-import iceCreamTubSource from "../assets/icecream_tub.png";
+import iceCreamTubPlasticSource from "../assets/iceCreamTubPlastic.png";
 import ketchupSource from "../assets/ketchup.png";
 import letterSource from "../assets/letter.png";
 import pizzaBoxSource from "../assets/pizza_box.png";
 import plasticBagSource from "../assets/plastic_bag.png";
 import popsicleStickSource from "../assets/popsicle_stick.png";
 import toiletRollSource from "../assets/toilet_roll.png";
+import iceCreamTubCardboardSource from "../assets/iceCreamTubCardboard.png";
+import coffeeCupOnlyCupSource from "../assets/coffeeCupOnlyCup.png";
+import coffeeCupOnlyLidSource from "../assets/coffeeCupOnlyLid.png";
 
 const ItemTypes = {
   can: canSource,
@@ -27,13 +30,16 @@ const ItemTypes = {
   chips: chipsSource,
   coffeeCup: coffeeCupSource,
   greasyPizzaBox: greasyPizzaBoxSource,
-  iceCreamTub: iceCreamTubSource,
+  iceCreamTubPlastic: iceCreamTubPlasticSource,
   ketchup: ketchupSource,
   letter: letterSource,
   pizzaBox: pizzaBoxSource,
   plasticBag: plasticBagSource,
   popsicleStick: popsicleStickSource,
   toiletRoll: toiletRollSource,
+  iceCreamTubCardboard: iceCreamTubCardboardSource,
+  coffeeCupOnlyCup: coffeeCupOnlyCupSource,
+  coffeeCupOnlyLid: coffeeCupOnlyLidSource,
 };
 
 export default ItemTypes;
