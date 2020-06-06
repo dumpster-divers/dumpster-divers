897539e2206673ae9bec8b69c7b3016319f6d8e8
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-06-07 01:32:00 +1000

    Add "addUsers" unit tests

--- controllers/userController.test.js
+++ controllers/userController.test.js
@@ -1,27 +1,25 @@
 const userController = require("./userController");
 jest.mock("../models/Users");
+jest.mock("gfycat-style-urls");
 
 //Shared
 
-saveFn = jest.fn();
-
-const UserModel = {
-  save: saveFn,
-};
-
 beforeAll(() => {
-  const Users = require("../models/Users");
-  Users.mockImplementation(() => {
-    return UserModel;
-  });
+  const gfy = require("gfycat-style-urls");
+  gfy.generateCombination = jest.fn().mockReturnValue("gfyusername");
 });
 
-// A simple example test
-
-const myMock = jest.fn();
-
 describe("add user", () => {
-  it(" saves user once", async () => {
+  test("returns correct body when successful add", async () => {
+    const MockUsers = {};
+    MockUsers.save = jest
+      .fn()
+      .mockImplementation((callback) => callback(false, {}));
+
+    const Users = require("../models/Users");
+    Users.exists = jest.fn().mockReturnValue(false);
+    Users.mockImplementation(() => MockUsers);
+
     const req = {
       body: {
         name: "name",
@@ -31,13 +29,48 @@ describe("add user", () => {
         username: "username",
       },
     };
-    const sendFn = myMock();
 
-    const res = {
-      send: sendFn,
-    };
+    const res = {};
+    res.send = jest.fn();
 
     await userController.addUser(req, res);
-    expect(saveFn.mock.calls.length).toBe(1);
+
+    const expected = {
+      name: "name",
+      username: "gfyusername",
+    };
+    expect(res.send.mock.calls[0][0]).toEqual(expected);
+  });
+
+  test("returns error body when unsuccessfully adds", async () => {
+    const MockUsers = {};
+    MockUsers.save = jest
+      .fn()
+      .mockImplementation((callback) => callback("error message", {}));
+
+    const Users = require("../models/Users");
+    Users.exists = jest.fn().mockReturnValue(false);
+    Users.mockImplementation(() => MockUsers);
+
+    const req = {
+      body: {
+        name: "name",
+        dateJoined: 0,
+        processedTotal: 100,
+        processedRecord: 100,
+        username: "username",
+      },
+    };
+
+    const res = {};
+    res.send = jest.fn();
+    res.status = jest.fn();
+    res.render = jest.fn();
+
+    await userController.addUser(req, res);
+    expect(res.send.mock.calls.length).toBe(0);
+    expect(res.status.mock.calls[0][0]).toBe(500);
+    expect(res.render.mock.calls[0][0]).toEqual("error");
+    expect(res.render.mock.calls[0][1]).toEqual({ error: "error message" });
   });
 });
