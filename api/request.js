2a1e65ea31b48cd9fd7e8bd86dc34870cae51d9c
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-06-06 22:53:49 +1000

    Yan: Add unit tests

---
+++ api/request.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+const baseURL = "http://127.0.0.1:5000";
+
+const request = axios.create({
+  baseURL,
+  timeout: 0,
+});
+
+request.interceptors.response.use(
+  (result) => {
+    const data = result.data;
+
+    return data;
+  },
+  (error) => Promise.reject(error)
+);
+
+module.exports = request;
